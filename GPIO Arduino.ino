db0f39818066d130d3a0c643ab245e7545c63e58
mzidan <48147058+zidanmzar1@users.noreply.github.com>
2023-04-16 22:10:06 +0700

    Test GPIO ESP32
    This is my first time i try microcontroller ESP32 and i test the pin

---
+++ GPIO Arduino.ino
@@ -0,0 +1,36 @@
+const int button1=4; // untuk button
+const int button2=13; // untuk button
+const int  A=16; // lampu hijau
+const int  B=17; // lampu merah
+
+ 
+void setup() {
+  // put your setup code here, to run once:
+pinMode (button1,INPUT_PULLUP); // posisi logika 1
+pinMode (button2,INPUT_PULLUP); // posisi logika 1 dan membutuhkan 0 dari button
+pinMode (A,OUTPUT); // button
+pinMode (B,OUTPUT); // button
+Serial.begin(115200);
+
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int kondisiPB1 = digitalRead(button1); // manamai pengkondisian variabel
+int kondisiPB2 = digitalRead(button2); // manamai pengkondisian variabel
+
+if(kondisiPB1==LOW) // posisi mati
+{
+  digitalWrite(A,HIGH);
+  digitalWrite(B,LOW);
+  Serial.println("LED HIJAU MENYALA");
+}
+if(kondisiPB2==LOW)
+{
+ Serial.println("LED MERAH MENYALA");
+ digitalWrite(A,LOW);
+ digitalWrite(B,HIGH);    
+}
+  
+}
